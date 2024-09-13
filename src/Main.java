43f9a8252517f64b15c58d120f6268ceb87e1980
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2024-09-13 01:50:27 -0300

    Feat : challenge convertidor de temperatura

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("runing");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+
+        //System.out.println(media);
+        String incluido;
+        if (incluidoEnElPlan == true){
+            incluido = "Esta pelicula esta incluida junto con la subscripcion";
+        }else {
+            incluido = "Esta pelicula no esta incluida junto con la subscripcion";
+        }
+        String sinopsis = """
+                Matrix es una paradoja
+                La mejor pelicula del fin del milenio
+                Fue lanzada en :
+                """ + fechaDeLanzamiento + """ 
+                \n
+                """ +incluido;
+
+        System.out.println(sinopsis);
+
+        int clasificacion = (int)(media/ 2); //convertir media a tipo de dato int
+
+
     }
 }
